1c70c3f83750ba5d38730b64e13219cd63d52716
Francesco <francescofogliajj@gmail.com>
2021-01-15 15:55:21 +0100

    Commit #39

--- js/script.js
+++ js/script.js
@@ -4,19 +4,23 @@ var app = new Vue({
 
   data: {
 
-    headerLinksLeft: [
+    headerLinks: [
 
-      { class: "fas fa-clock", name: "Open Hours: Mon - Sat - 9:00 - 18:00", url: "#" }
+      {
+        links: [
+          { class: "fas fa-clock", name: "Open Hours: Mon - Sat - 9:00 - 18:00", url: "#" }
+        ]
+      },
 
-    ],
-
-    headerLinksRight: [
-
-      { class: "fas fa-phone-alt", name: "+1 (305) 1234-5678", url: "#" },
-      { class: "fas fa-envelope", name: "hello@example.com", url: "#" },
-      { class: "fab fa-facebook-f", name: "", url: "#" },
-      { class: "fab fa-twitter", name: "", url: "#" },
-      { class: "fab fa-linkedin-in", name: "", url: "#" }
+      {
+        links: [
+          { class: "fas fa-phone-alt", name: "+1 (305) 1234-5678", url: "#" },
+          { class: "fas fa-envelope", name: "hello@example.com", url: "#" },
+          { class: "fab fa-facebook-f", name: "", url: "#" },
+          { class: "fab fa-twitter", name: "", url: "#" },
+          { class: "fab fa-linkedin-in", name: "", url: "#" }
+        ]
+      }
 
     ],
 
